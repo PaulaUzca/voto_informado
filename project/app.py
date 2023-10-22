cbb36c9ebf7cdb18cf79e2190cbbe4a94621eee2
Paula Uzcategui <paula.uzcategui68@gmail.com>
2023-10-21 23:14:26 -0500

    jorge edit

--- project/app.py
+++ project/app.py
@@ -11,13 +11,11 @@ from sodapy import Socrata
 app = Flask(__name__)
 
 THIS_FOLDER = Path(__file__).parent.resolve()
-csv_file = THIS_FOLDER / 'static/data_nice.csv'
+csv_file = THIS_FOLDER / 'static/data_nice2.csv'
 df = pd.read_csv(csv_file, encoding = "iso-8859-1")
 
 #Sin los cargos de juntas administradoras locales porque no nos vamos a poner a buscar por comuna
 df = df[df['Descripción de la Corporación/Cargo'] != 'JUNTAS ADMINISTRADORAS LOCALES']
-df.fillna('', inplace=True)
-df['nombresApellidos'] = df['Primer Nombre'] + ' '  +df['Segundo Nombre'] + ' ' + df['Primer Apellido'] + ' ' + df['Segundo Apellido']
 
 @app.route('/')
 def hello_world():
@@ -132,7 +130,7 @@ def get_summary():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
 
